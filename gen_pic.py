49414b9706ffd1e63991b946f018f2a34e49f493
Paweł Bogaczewicz <pawel.bogaczewicz@outlook.com>
2017-02-26 23:10:32 +0100

    scoring done

--- gen_pic.py
+++ gen_pic.py
@@ -1,3 +1,5 @@
+#! /usr/bin/python3
+
 import random
 from threading import Thread
 
@@ -8,7 +10,14 @@ H = 382
 SZ = H * W
 data = bytes
 SPECIMENT_CNT = 4
-specimen = [0] * SPECIMENT_CNT
+specimens = [0] * SPECIMENT_CNT
+
+
+class Specimen():
+    def __init__(self, name='mona'):
+        self.data = [0] * SZ
+        self.score = -1
+        self.name = name
 
 
 class MutateHandler(Thread):
@@ -24,27 +33,42 @@ class MutateHandler(Thread):
         c = random.randint(0, 255)
         for n in range(y, h + y):
             for m in range(x, w + x):
-                self.specimen[n * W + m] = (self.specimen[n * W + m] + c) >> 1
-                # print(self.name)
+                self.specimen.data[n * W + m] = (self.specimen.data[n * W + m] + c) >> 1
+        im = Image.frombytes('P', (W, H), bytes(self.specimen.data))
+        im.save('/tmp/mona_sp_' + self.name + '.png')
+
 
 class ScoreHandler(Thread):
     def __init__(self, specimen):
         super().__init__()
         self.specimen = specimen
 
+    def run(self):
+        print('score ' + self.name + ' start')
+        _score = 0
+        for i in range(SZ):
+            a = data[i]
+            b = self.specimen.data[i]
+            _score += (a - b) ** 2
+        self.specimen.score = _score
+        print(self.name, self.specimen.name, str(self.specimen.score))
+        print('score ' + self.name + ' stop')
+
+
 def mutate():
     threads = list()
-    for i in range(SPECIMENT_CNT):
-        m = MutateHandler(specimen[i])
+    for specimen in specimens:
+        m = MutateHandler(specimen)
         m.start()
         threads.append(m)
     for m in threads:
         m.join()
 
+
 def score():
     threads = list()
     for i in range(SPECIMENT_CNT):
-        s = ScoreHandler(specimen[i])
+        s = ScoreHandler(specimens[i])
         s.start()
         threads.append(s)
     for s in threads:
@@ -59,10 +83,10 @@ if __name__ == '__main__':
     data = im.tobytes()
     im.close()
     for i in range(SPECIMENT_CNT):
-        specimen[i] = [0] * SZ
+        specimens[i] = Specimen(str(i))
     random.seed(100)
 
-    for i in range(10):
+    for i in range(2):
         mutate()
 
         score()
