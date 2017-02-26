2b74ffd4c34d0526e5152bfeb2d676f641974e07
Paweł Bogaczewicz <pawel.bogaczewicz@outlook.com>
2017-02-26 22:25:23 +0100

    multi thread mutate

--- gen.py
+++ gen.py
@@ -1,37 +1,55 @@
 import random
+from threading import Thread
+
 from PIL import Image
 
 W = 256
 H = 382
 SZ = H * W
 data = bytes
-SPECIMENT_CNT = 4
-specimen = [0] * SZ
-specimens = list()
+SPECIMENT_CNT = 40
+specimen = [0] * SPECIMENT_CNT
 
-if __name__ == '__main__':
-    with open('mona_small_gray.raw', 'rb') as f:
-        picture = f.read()
 
-    im = Image.open('mona_small_gray.png')  # type: Image.Image
-    # print(im.mode, im.size)
-    data = im.tobytes()
-    for i in range(SPECIMENT_CNT):
-        specimens.append(specimen)
-    random.seed(100)
+class MutateHandler(Thread):
+    def __init__(self, specimen):
+        super().__init__()
+        self.specimen = specimen
 
-    for i in range(0, SPECIMENT_CNT):
+    def run(self):
         x = random.randint(0, W)
         y = random.randint(0, H)
-        w = random.randint(x, W) + 1
-        h = random.randint(y, H) + 1
+        w = random.randint(0, W - x)
+        h = random.randint(0, H - y)
         c = random.randint(0, 255)
-        for n in range(y, h):
-            for m in range(x, w):
-                specimens[i][n * W + m] = (specimens[i][n * W + m] + c) >> 1
+        for n in range(y, h + y):
+            for m in range(x, w + x):
+                self.specimen[n * W + m] = (self.specimen[n * W + m] + c) >> 1
+        print(self.name)
+
+def mutate():
+    threads = list()
 
     for i in range(0, SPECIMENT_CNT):
-        im2 = Image.frombytes('P', (W, H), bytes(specimens[i]))
-        im2.show()
+        m = MutateHandler(specimen[i])
+        m.start()
+        threads.append(m)
 
-        # im2.save('mona2.png')
+    for m in threads:
+        m.join()
+
+if __name__ == '__main__':
+    # with open('mona_small_gray.raw', 'rb') as f:
+    #     picture = f.read()
+
+    im = Image.open('mona_small_gray.png')  # type: Image.Image
+    data = im.tobytes()
+    im.close()
+    for i in range(SPECIMENT_CNT):
+        specimen[i] = [0] * SZ
+    random.seed(100)
+
+    mutate()
+
+
+
